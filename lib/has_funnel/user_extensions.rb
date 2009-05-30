249b8f5046b233a3756890316ccd8f2db5005dc8
Joshua Krall <josh@transfs.com>
2009-05-30 00:17:09 -0500

    Adding check for nil visitor in user_extensions

--- lib/has_funnel/user_extensions.rb
+++ lib/has_funnel/user_extensions.rb
@@ -1,15 +1,15 @@
 
 module FunnelCake
   module HasFunnel
-    
+
     module UserExtensions
       def self.included( recipient )
         recipient.extend( ClassMethods )
       end
-      
+
       module ClassMethods
         def has_funnel(opts={})
-          
+
           # Set up the FunnelVisitor model association
           opts[:visitor_class_name] = 'FunnelVisitor' if opts[:visitor_class_name].nil?
           params = {:class_name=>opts[:visitor_class_name], :dependent=>:destroy}
@@ -20,19 +20,19 @@ module FunnelCake
           include FunnelCake::HasFunnel::UserExtensions::InstanceMethods
         end
       end
-      
+
       module InstanceMethods
-        
+
         # Utility method for logging funnel events from within application code
         # (This is probably how most funnel events will be triggered)
         # - First check if the event is legal... if not, log an error I guess!
         # - Second, send() the event method with the accompanying data
         def log_funnel_event(event, data={})
-          self.visitor.log_funnel_event(event, data)
+          self.visitor.log_funnel_event(event, data) unless self.visitor.nil?
         end
-        
+
       end
     end
-    
+
   end
 end
