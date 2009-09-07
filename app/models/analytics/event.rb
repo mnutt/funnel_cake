c8c77c4d6f4889b42889d98cd9b0526e9d2e9e08
Joshua Krall <josh@transfs.com>
2009-09-07 08:45:32 -0500

    Revised sorting of events in visitor :show page

--- app/models/analytics/event.rb
+++ app/models/analytics/event.rb
@@ -5,4 +5,5 @@ class Analytics::Event < ActiveRecord::Base
   belongs_to :visitor, :class_name=>'Analytics::Visitor', :foreign_key=>:visitor_id
 
   named_scope :sorted_by_date, :order=>'created_at DESC'
+  named_scope :sorted, :order=>'id DESC'
 end
