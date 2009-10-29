b05023ae346c3826afc9fa5062bf64961526e344
Joshua Krall <krall@JoshMacPro.local>
2009-10-29 02:19:17 -0500

    Revised layout of funnel diagram, cleaned up formatting, reduced font/graph size.

--- app/helpers/analytics/events_helper.rb
+++ app/helpers/analytics/events_helper.rb
@@ -3,11 +3,11 @@ module Analytics::EventsHelper
   # Grab raw position data from the diagram xdot
   XDOT_FILE = File.join(RAILS_ROOT, 'app', 'views', 'analytics', 'events', '_diagram.xdot.erb')
   RAW_STATE_POSITIONS_ARR = File.open(XDOT_FILE).read.split(/\n/).
-                          collect { |l| l.match(/(\w+) \[label=\".*pos=\"(\d+,\d+)\"/) }.
+                          collect { |l| l.match(/(\w+) \[label=\".*pos=\"([\d\.]+,[\d\.]+)\"/) }.
                           delete_if {|m| m.nil?}.
                           collect {|m| [m[1].to_sym, {:left=>m[2].split(/,/)[0].to_i, :top=>m[2].split(/,/)[1].to_i}] }
   RAW_STATE_POSITIONS = Hash[*RAW_STATE_POSITIONS_ARR.flatten]
-  state_pos_bounds_matches = File.open(XDOT_FILE).read.match(/bb=\"0,0,(\d+),(\d+)\"/ms)
+  state_pos_bounds_matches = File.open(XDOT_FILE).read.match(/bb=\"0,0,([\d\.]+),([\d\.]+)\"/ms)
   STATE_POSITION_BOUNDS = {:width=>state_pos_bounds_matches[1].to_i, :height=>state_pos_bounds_matches[2].to_i}
   STATE_POSITIONS = RAW_STATE_POSITIONS.each_value{ |h| h[:top] = STATE_POSITION_BOUNDS[:height] - h[:top]; h }
   POS_PADDING = 8
