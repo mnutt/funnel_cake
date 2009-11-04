d1f9b8b99525796eedfe0a38d87f6e488d0c1068
Joshua Krall <krall@JoshMacPro.local>
2009-11-04 08:04:29 -0600

    Adding requires for additional source files to init.rb

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require File.dirname(__FILE__) + '/lib/funnel_cake/data_hash'
+require File.dirname(__FILE__) + '/lib/funnel_cake/state_period_helpers'
 require File.dirname(__FILE__) + '/lib/funnel_cake/engine'
 require File.dirname(__FILE__) + '/lib/funnel_cake/random_id'
 require File.dirname(__FILE__) + '/lib/acts_as_funnel_state_machine'
@@ -5,7 +7,7 @@ require File.dirname(__FILE__) + '/lib/has_funnel/user_extensions'
 require File.dirname(__FILE__) + '/lib/has_visitor_tracking/controller_extensions'
 
 ActiveRecord::Base.class_eval do
-  include ScottBarron::Acts::FunnelStateMachine  
+  include ScottBarron::Acts::FunnelStateMachine
   include FunnelCake::HasFunnel::UserExtensions
 end
 
