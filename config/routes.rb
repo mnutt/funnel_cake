e2091e43b6d40d602185321d2964c9bc9fdecb68
Josh Krall <krall@JoshMacPro.local>
2009-12-12 11:30:37 -0600

    Updated routes file with latest routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,20 @@ ActionController::Routing::Routes.draw do |map|
 
   # Uncomment these, or copy them to your own config/routes.rb
   # map.namespace :analytics do |analytics|
-  #   analytics.resources :events, {:collection=>{:diagram=>:get, :stats_graph=>:get}}
+  #   analytics.resources :events
+  #   analytics.resources :conversions, :member=>{:history=>:get, :visitors=>:get}
+  #   analytics.resources :states, :member=>{:visitors=>:get}
   #   analytics.resources :ignores
+  #   analytics.resources :visitors
+  #   analytics.resources :stats
+  #   analytics.resources :dashboards, :collection=>{
+  #     :main=>:get,
+  #     :cec=>:get,
+  #     :diagram=>:get,
+  #     :overview=>:get,
+  #     :clear_cache=>:get,
+  #     :customers=>:get,
+  #   }
   # end
 
 end
